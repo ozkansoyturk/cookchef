805777dd3d9631bab264664b33b5b7ee18064409
ozkansoyturk <soyturkozkan@gmail.com>
2023-06-30 18:43:19 +0200

    Mise en place du Header et du Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
+import styles from "./Footer.module.scss";
+
 function Footer() {
-  return <div className="b3">Footer</div>;
+  return (
+    <footer
+      className={`${styles.footer} d-flex flex-row align-items-center justify-content-center p-20`}
+    >
+      <p>Copyright © 2022 Cookchef Dyma, Inc.</p>
+    </footer>
+  );
 }
 
 export default Footer;
