5de322966ab0db0c02bd79a746a1da0f2d519be3
ozkansoyturk <soyturkozkan@gmail.com>
2023-06-30 17:20:04 +0200

    Mise en page de l'application

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import styles from "./App.module.scss";
+
 function App() {
-  return <h1>Cookchef</h1>;
+  return (
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <div className="b3">Footer</div>;
+}
+
+export default Footer;
