726853280b85325c3bdbdff2328b99842a668c74
ozkansoyturk <soyturkozkan@gmail.com>
2023-09-10 01:09:37 +0200

    Mise en place du formulaire

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
+import { useState } from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
+import Admin from "./pages/Admin/Admin";
 import styles from "./App.module.scss";
 // import { seedRecipes } from "./data/seed";
 
 // seedRecipes();
 
 function App() {
+  const [page, setPage] = useState("homepage");
+
   return (
     <div className={`d-flex flex-column ${styles.appContainer}`}>
-      <Header />
-      <HomePage />
+      <Header setPage={setPage} />
+      {page === "homepage" && <HomePage />}
+      {page === "admin" && <Admin />}
       <Footer />
     </div>
   );
 }
-
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,19 +2,26 @@ import { useState } from "react";
 import styles from "./Header.module.scss";
 import HeaderMenu from "./components/HeaderMenu";
 
-function Header() {
+function Header({ setPage }) {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
     <header className={`${styles.header} d-flex flex-row align-items-center`}>
       <div className="flex-fill">
         <img
+          onClick={() => setPage("homepage")}
           src="https://github.com/dymafr/react-c5l4/blob/master/src/assets/images/cookchef.png?raw=true"
           alt="logo cookchef"
         />
       </div>
       <ul className={styles.headerList}>
-        <button className="mr-5 btn btn-reverse-primary">
+        <button
+          onClick={() => setPage("admin")}
+          className="btn btn-primary mr-15"
+        >
+          Ajouter une recette
+        </button>
+        <button className="mr-15 btn btn-reverse-primary">
           <i className="fa-solid fa-heart mr-5"></i>
           <span>Wishlist</span>
         </button>
