e0cfa449ccbbff68925230ad2c22aabb79cde5fd
ozkansoyturk <soyturkozkan@gmail.com>
2023-09-26 02:26:52 +0200

    Installation et configuration de TypeScript + renommage des files

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./assets/styles/index.scss";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
